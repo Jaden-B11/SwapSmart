e0302b6e39976d974cd5ee8f21ff9517ab060d3c
jeslynsee <jeslynesee@gmail.com>
2026-02-26 15:51:05 -0800

    Finish basic layout and logo on Options page

--- app/(tabs)/options.tsx
+++ app/(tabs)/options.tsx
@@ -1,11 +1,55 @@
-import { View } from "react-native";
+// @ts-nocheck
+import { useRouter } from "expo-router";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Header from "../../components/ui/Header.tsx";
 
 export default function Options() {
-
+    const router = useRouter();
 
     return (
-        <View>
-            
+        <View style={styles.container}>
+            {/* SwapSmart Logo and Name */}
+            <Header/>
+
+            {/* Options for Login or Signup */}
+            <View style={styles.optionsContainer}>
+                <TouchableOpacity style={styles.buttonOptions} onPress={() => router.replace("/auth/login")}>
+                    <Text style={styles.buttonText}>Login</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.buttonOptions} onPress={() => router.replace("/auth/sign-up")}>
+                    <Text style={styles.buttonText}>Sign Up</Text>
+                </TouchableOpacity>
+
+            </View>
+
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#f5f5f5",
+        marginTop: 100
+    },
+    optionsContainer: {
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 16,
+        padding: 24,
+        marginTop: 32
+    },
+    buttonOptions: {
+        alignItems: "center",
+        borderRadius: 8,
+        paddingVertical: 12,
+        backgroundColor: "#007AFF",
+        width: "80%"
+    },
+    buttonText: {
+        color: "#fff",
+        fontSize: 16,
+        fontWeight: "600"
+    }
+});
